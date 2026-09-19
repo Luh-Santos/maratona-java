85683581b25f96444c500abddea0738272bb83e8
Luh Santos <luisasasilva19@gmail.com>
2026-09-19 15:11:51 -0300

    feat: aula 01 - introdução e comentários

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b> <br>
+ * Ctrl + Alt + Q para renderizar esse comentário
+ */
+public class Aula01OlaDevDojo {
+    // Comentário linha
+
+    /*
+    Comentário
+    De
+    Múltiplas
+    Linhas
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo");
+    }
+}
