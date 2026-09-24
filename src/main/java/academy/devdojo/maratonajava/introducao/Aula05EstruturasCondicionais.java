cd58e098455fd7a1c3d1d2fbe9e28d3d25b065d9
Luh Santos <luisasasilva19@gmail.com>
2026-09-24 01:33:35 -0300

    feat: Aula 05 pt 01 - Estruturas Condicionais - IF

---
+++ src/main/java/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais {
+    public static void main(String[] args) {
+        // if precisa ter dentro do parênteses um booleano
+        if (true) {
+            System.out.println("Dentro do if");
+        }
+        System.out.println("Fora do if");
+        /* se tiver apenas 1 linha de instrução dentro do if, não precisa das chaves, e pode deixar na mesma linha se quiser
+        if (true)
+            System.out.println("Dentro do if sem chaves");
+
+        if (true) System.out.println("Dentro do if sem chaves");
+        */
+
+        int idade = 20;
+        boolean isAutorizadoComprarBebida = idade >= 18;
+        if (isAutorizadoComprarBebida) {
+            System.out.println("Autorizado a comprar bebida alcoólica");
+        }
+        // Se isAutorizadoComprarBebida NÃO for verdadeiro, então...
+        if (!isAutorizadoComprarBebida) {
+            System.out.println("Não autorizado a comprar bebida alcoólica");
+        }
+
+
+
+    }
+}
